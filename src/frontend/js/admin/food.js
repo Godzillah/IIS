5718962b99d7addc5412fcb28f17930b610b1abb
Seequick1 <xorsak02@stud.fit.vutbr.cz>
2018-11-27 21:40:40 +0100

    [MO] -> nothing changed

--- src/frontend/js/admin/food.js
+++ src/frontend/js/admin/food.js
@@ -365,7 +365,7 @@ function controlFoodInputs(new_type, new_description, new_price, counterOfTheRow
         }
         if(!$selectedLength.match($regexForInputIngredients)){
             $('#ingredients_row_' + counterOfTheRows+'').append("<div " + "id='badSelect"+counterOfTheRows+"'>Wrong select (at least 1 select)</div>");
-            $('#badSelect' + counterOfTheRows+'').addClass("alert alert-danger text-danger font-weight-bold text-center"); 
+            $('#badSelect' + counterOfTheRows+'').addClass("alert alert-danger text-danger font-weight-bold text-center");  
         }
     }
 }
